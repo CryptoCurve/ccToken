8b795613806e85385f5eb0e33311c7f2f39aa181
anton <anton@mfin.co.za>
2018-07-23 08:29:16 +0200

    WRC20 based Zeppelin contracts added to repo. Unit Tests for basic functionality of curveToken and CurveCrowdsale. Added a cap to the crowdsale functionality. Modified deployment to function properly.
    Signed-off-by: anton <anton@mfin.co.za>

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,26 +1,25 @@
-
-var CurveCrowdsale = artifacts.require("CurveCrowdsale.sol");
 var CurveToken = artifacts.require("CurveToken.sol");
+var CurveCrowdsale = artifacts.require("./CurveCrowdsale.sol");
 
-const c = {
-  _openingTime: '1531389600', //2018-07-12T10:00:00
-  _closingTime: '1531390200', //2018-07-12T10:10:00
-  _rate: '1', //number of token unites the buyer gets per wei
-  _wallet: '0x81600316b99c8e77c106F93b28b93e751d9BBFe5', //wallet that the funds will be deposited to
-  _token: '' //address of the token being sold (acquired from the deployed token contract)
-}
+module.exports = function(deployer, network, accounts) {
+  const startTime = Math.round((new Date(Date.now() + 5000).getTime())/1000); // 5 seconds from now
+  const endTime = Math.round((new Date(Date.now() + 10000).getTime())/1000); // 10 seconds from now        //Math.round((new Date().getTime() + (86400000 * 20))/1000); // Today + 20 days
+  const rate = new web3.BigNumber(1000);
+  const cap = new web3.BigNumber(web3.toWei(10, "ether"));
+  const wallet = accounts[4];
 
-module.exports = function(deployer) {
-  deployer.deploy(CurveToken)
-  .then(() => {
-    deployer.deploy(CurveCrowdsale,
-      c._openingTime,
-      c._closingTime,
-      c._rate,
-      c._wallet,
-      CurveToken.address)
-      .then(() => {
-        console.log("OK")
-      }).catch(errOut('MIGRATE: error deploying CurveCrowdsale'));;
-  }).catch(errOut('MIGRATE: error deploying CurveToken'));
+  return deployer
+    .then(() => {
+      return deployer.deploy(CurveToken);
+    })
+    .then(() => {
+      return deployer.deploy(CurveCrowdsale,
+        startTime,
+        endTime,
+        rate,
+        wallet, // Replace this wallet address with the last one (10th account) from Ganache UI. This will be treated as the beneficiary address. 2000000000000000000, // 2 ETH 500000000000000000000 // 500 ETH
+        cap,
+        CurveToken.address
+      )
+    })
 };
